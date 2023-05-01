492bb6f64da11dd4435e06e51cd20e7af55a0b18
fgbarrios <fg1cazares@gmail.com>
2023-05-01 17:00:04 -0300

    nuevas rutas

---
+++ src/lib/info.js
@@ -0,0 +1,18 @@
+export const links = [
+    {
+      nombre: 'Nosotros',
+      slug: 'nosotros',
+    },
+    {
+      nombre: 'Tiendas',
+      slug: 'tiendas',
+    },
+    {
+      nombre: 'Blog',
+      slug: 'blog',
+    },
+    {
+      nombre: 'FAQ',
+      slug: 'faq',
+    }
+]
